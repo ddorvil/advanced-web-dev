a8dc1dbaa41991b1a053bf151e154f8ffea09837
ddorvil <56710675+ddorvil@users.noreply.github.com>
2020-05-24 16:56:35 -0400

    Add files via upload

---
+++ fassignment02/inc/aacunanav.php
@@ -0,0 +1,25 @@
+<nav class="menu">
+
+		<ul>
+
+			<li><a href="index.php">Home</a></li>
+
+			<li><a href="aacuna-main.php" class="is-current">Ana Sofía Acuña</a></li>
+
+			<li><a href="jhuo3-main.php">Jialin Huome</a></li>
+
+			<li><a href="anels28-main.php">Alyssa L Nelson</a></li>
+
+		</ul>
+
+</nav>
+
+
+	<nav class = "selfmenu">
+		<ul>
+			<li><a href="aacuna-main.php">Origins</a></li>
+			<li><a href="aacuna-journey.php">Journey</a></li>
+			<li><a href="aacuna-today.php">Today</a></li>
+			<li><a href="aacuna-tomorrow.php">Tomorrow</a></li>
+		</ul>
+	</nav>
---
+++ fassignment02/inc/anels28-intro.php
@@ -0,0 +1,49 @@
+<body class="container">
+	<header>
+		<a href="index.php">
+			<h1>Alyssa L Nelson</h1>
+			<img src="images/moi.jpg" alt="moi">
+		</a>
+	</header>
+
+	<aside>
+		<ul>
+			<li>Double major, History and GSW</li>
+			<li>Class of 2021</li>
+			<li>Avid theatre lover</li>
+		</ul>
+		
+	</aside>
+
+	<nav class="menu">
+
+		<ul>
+
+			<li><a href="index.php">Home</a></li>
+
+			<li><a href="aacuna-main.php">Ana Sofía Acuña</a></li>
+
+			<li><a href="jhuo3-main.php">Jialin Huome</a></li>
+
+			<li><a href="anels28-main.php" class="is-current">Alyssa L Nelson</a></li>
+
+		</ul>
+
+</nav>
+
+
+<nav class = "selfmenu">
+
+		<ul>
+
+			<li><a href="anels28-main.php">Home</a></li>
+
+			<li><a href="anels28-highschool.php">High School</a></li>
+
+			<li><a href="anels28-college.php">Early College</a></li>
+
+			<li><a href="anels28-now.php">Now</a></li>
+
+		</ul>
+
+</nav>
